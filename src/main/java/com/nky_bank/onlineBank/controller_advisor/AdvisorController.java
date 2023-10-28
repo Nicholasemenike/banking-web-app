91c73bc68cd826641999b3e142a1833cd19674e0
NicholasEmenike <emenikenicholas022@gmail.com>
2023-10-28 18:12:36 +0100

    another commit

--- src/main/java/com/nky_bank/onlineBank/controller_advisor/AdvisorController.java
+++ src/main/java/com/nky_bank/onlineBank/controller_advisor/AdvisorController.java
@@ -1,8 +1,9 @@
 package com.nky_bank.onlineBank.controller_advisor;
 
-import com.nky_bank.onlineBank.models.User;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.nky_bank.onlineBank.models.User;
 
 @ControllerAdvice
 public class AdvisorController {
